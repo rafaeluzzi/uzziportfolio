75341b6ed36b85cda985cfc6a1f4a39368cb4a63
rafaeluzzi <rafaeluzzi@gmail.com>
2025-04-30 11:52:08 -0400

    dark theme spotify play

--- src/components/NowPlaying.tsx
+++ src/components/NowPlaying.tsx
@@ -43,14 +43,12 @@ export function NowPlaying() {
     <div className="w-full">
       <iframe
         style={{ borderRadius: '12px' }}
-        src={track.embedUrl}
+        src={track.embedUrl+'?utm_source=generator&theme=0'}
         width="100%"
         height="152"
-        frameBorder="0"
         allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"
         loading="lazy"
       ></iframe>
     </div>
   )
 }
-
